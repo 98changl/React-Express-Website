286bba88585db381d7068d3828d82c3501bca4a1
98changl <98changl@gmail.com>
2023-03-22 22:36:36 -0400

    Fixed multiple proxies
    Resolved issue with setupProxy.js only using localhost for proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/*', { target: 'http://localhost:3001' }));
-    app.use(createProxyMiddleware('/*', { target: 'https://limanchangnode.onrender.com', changeOrigin: true }));
+    app.use(createProxyMiddleware('/local', {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/local': ''
+        }
+    }));
+    app.use(createProxyMiddleware('/*', {
+        target: 'https://limanchangnode.onrender.com',
+        changeOrigin: true
+    }));
 }
