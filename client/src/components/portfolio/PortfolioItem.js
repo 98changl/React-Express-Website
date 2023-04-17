b3d2b1d694b7d228cbbe5af13a8ee6533d66903e
98changl <98changl@gmail.com>
2023-04-17 17:46:47 -0400

    Updated Portfolio

--- client/src/components/portfolio/PortfolioItem.js
+++ client/src/components/portfolio/PortfolioItem.js
@@ -1,21 +1,18 @@
 function PortfolioItem(props) {
     return (
         <li className='container-fluid mb-5'>
-            <div className='grid text-center'>
-
+            <div className='card p-3 grid text-center'>
                 <div className='row align-items-center'>
-                    <div className='col-md-3'>
+                    <div className='col-md-6'>
                         <div className=''>
                             <h2 className='subtitle text-center fw-bold'>{props.title}</h2>
-                            <p className='text text-center fst-italic'>{props.date}</p>
+                            <span className='text text-center fw-bold'>Technologies Used: </span>
+                            <span className='text text-center fst-italic'>{props.skills}</span>
+                            <br /><br />
+                            <p className='text text-start'>{props.description}</p>
                         </div>
                     </div>
-                    <div className='col-md-4'>
-                        <p className='text text-center fw-bold'>Technologies Used:</p>
-                        <p className='text text-center fst-italic'>{props.skills}</p>
-                        <p className='text text-start'>{props.description}</p>
-                    </div>
-                    <div className='col-md-5'>
+                    <div className='col-md-6'>
                         <a className='text-center' href={props.link} target='_blank' rel='noreferrer'>
                             <img className='portfolioimg' src={props.src} alt={props.title} />
                         </a>
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -3,6 +3,8 @@ import PortfolioList from "../components/portfolio/PortfolioList";
 import p1img from '../media/p1.JPG'
 import p2img from '../media/p2.JPG'
 import p3img from '../media/p3.JPG'
+import p4img from '../media/p4.JPG'
+import p5img from '../media/p5.JPG'
 
 const items = [
     {
@@ -10,8 +12,8 @@ const items = [
         title: 'Luna Web Application',
         date: 'Jan - June 2021',
         link: 'https://github.com/98changl',
-        skills: 'Amazon Web Services, CSS, Django, Git, HTML, Jira, PostgreSQL',
-        description: 'Social media web application for users to engage in the discussion of trading and stock positions.',
+        skills: 'Amazon Web Services, CSS, Django, Git, HTML, Jira, PostgreSQL, Python',
+        description: 'A social media web application for the stock market that helps users discuss information about their stock positions with other users, allowing easier and more informed trading decisions.',
         src: p1img
     },
     {
@@ -19,8 +21,8 @@ const items = [
         title: 'Online Railway Booking System',
         date: 'Nov - Dec 2020',
         link: 'https://github.com/98changl',
-        skills: 'Amazon Web Services, Git, Java, JavaServer Pages, MySQL, Tomcat',
-        description: 'Web app managing tickets and train schedules. Users possess the ability to make reservations for train lines.',
+        skills: 'Amazon Web Services, Git, HTML, Java, JavaServer Pages, MySQL, Tomcat',
+        description: 'Web application utilizing a relational database system to simulate a railway booking system. Customers possess the ability to make or cancel reservations for train lines and see past reservations. Administrators possess the ability to edit customer representatives, obtain sales reports, produce a list of reservations and revenue, and retrieve the highest grossing transit line. Any user can check the train schedules listed within the database.',
         src: p2img
     },
     {
@@ -29,8 +31,26 @@ const items = [
         date: 'Dec 2020',
         link: 'https://github.com/98changl/CS-428/tree/master/P1',
         skills: 'C#, Git, Photon Engine, Unity 3D',
-        description: 'An online multiplayer game of tag. Set within a large map with moving obstacles and contains player characters with animations',
+        description: 'An online multiplayer game of tag. Set within a large map with moving obstacles and contains player characters with animations tied to player actions.',
         src: p3img
+    },
+    {
+        id: 'p4',
+        title: 'Linear Filter',
+        date: 'Oct 2020',
+        link: 'https://github.com/98changl/CS-334/blob/main/src/A2_Q2_4.java',
+        skills: 'ImageJ, Java',
+        description: 'ImageJ plugin that uses a linear filter to create a horizontal blur over a length of 7 pixels. The plugin is used to simulate the effect of camera movement.',
+        src: p4img
+    },
+    {
+        id: 'p5',
+        title: 'Museum Tickets',
+        date: 'Dec 2020',
+        link: 'https://github.com/98changl/CS-213/tree/master/project5',
+        skills: 'Android Studio, Java',
+        description: 'Android app that can estimate the ticket prices for admissions to a list of museums.',
+        src: p5img
     }
 ];
 
